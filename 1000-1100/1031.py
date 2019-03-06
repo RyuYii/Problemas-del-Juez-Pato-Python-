77f7f004235222447e1c439bb1df0e51a490f91a
RyuYii <ruddysurco30@gmail.com>
2019-03-06 13:35:35 -0400

    Problema: Mediana(corregido)

--- 1000-1100/1031.py
+++ 1000-1100/1031.py
@@ -1,20 +1,24 @@
 """Problema: Mediana
-no corre en el juez virtual porque falta el metodo hasnext
+corregido
 """
-n = int(input())    #leemos la catidad de numeros
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        n = int(line)    #leemos la catidad de numeros
 
 #como los n numeros vienen en una linea procedemos con lo siguiente
-m = input()
-v = m.split()
-c = []
-for i in v:
-    c.append(int(i))
+        m = input()
+        v = m.split()
+        c = []
+        for i in v:
+            c.append(int(i))
 #como resultado los datos estaran almacenados en un vector de enteros
-c.sort()    #ordenamos el vector
+        c.sort()    #ordenamos el vector
 
 #verificamos lo que se nos pide
-if(n%2!=0):
-    pos = n//2
-    print(c[pos])
-else:
-    print(-1)
+        if(n%2!=0):
+            pos = n//2
+            print(c[pos])
+        else:
+            print(-1)
