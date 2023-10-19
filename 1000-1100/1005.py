ed59c4106a3ca74127bd4c6aaf6e029a81a903ef
RyuYii <ruddysurco30@gmail.com>
2023-10-18 21:20:33 -0400

    Problema: Bob el jardinero (actualizado)

--- 1000-1100/1005.py
+++ 1000-1100/1005.py
@@ -1,17 +1,35 @@
 """Bob el jardinero"""
+# de hecho se cambio por el estilo de redondeo de C, pero me dio flojera actualizar nombres :v
+def round_java_style(numero, decimales=2):
+    return int(numero * 100 + 0.5) / 100
 
-casos = int(input())
-for n in range(0, casos):
-    cadena = input()
-    a = cadena.count("a")
-    e = cadena.count("e")
-    i = cadena.count("i")
-    o = cadena.count("o")
-    u = cadena.count("u")
+def calcular_porcentaje_de_frutas(cadena):
+    frutas = 'aeiou'
     total = len(cadena)
-    print("Caso {0}:".format(n+1))
-    print("a=", "{0:.2f}".format((a * 100) / total))
-    print("e=", "{0:.2f}".format((e * 100) / total))
-    print("i=", "{0:.2f}".format((i * 100) / total))
-    print("o=", "{0:.2f}".format((o * 100) / total))
-    print("u=", "{0:.2f}".format((u * 100) / total))
+    
+    porcentajes = {}
+    for fruta in frutas:
+        count = cadena.count(fruta)
+        porcentaje = (count * 100) / total
+        porcentajes[fruta] = round_java_style(porcentaje)
+    
+    return porcentajes
+
+# Función principal para procesar múltiples casos de prueba
+def procesar_casos_de_prueba(casos):
+    c = 1
+    for caso in casos:
+        resultado = calcular_porcentaje_de_frutas(caso)
+        print(f'Caso {c}:')
+        c += 1
+        for fruta, porcentaje in resultado.items():
+            print(f"{fruta}= {porcentaje:.2f}")
+
+if __name__ == "__main__":
+    T = int(input())
+    casos = []
+    for _ in range(T):
+        cadena = input()
+        casos.append(cadena)
+    
+    procesar_casos_de_prueba(casos)
